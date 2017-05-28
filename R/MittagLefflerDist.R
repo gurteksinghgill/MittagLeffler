704af09e233e03ad5f8267fa9ba0860772ebff6a
Peter Straka <p.straka@unsw.edu.au>
2017-05-28 20:25:40 +1000

    Added R code for second type Mittag-Leffler

--- R/MittagLefflerDist.R
+++ R/MittagLefflerDist.R
@@ -12,30 +12,49 @@
 #' @param log,log.p logical; if TRUE, probabilities p are given as log(p)
 #' @param lower.tail logical; if TRUE, probabilities are P(X ≤ x) 
 #'     otherwise, P(X > x)
+#' @param second.type logical; if FALSE, first type of Mittag-Leffler
+#'     distribution is assumed
 #' @details
-#' The generalized Mittag-Leffer function is defined by the power series 
+#' The generalized (two-parameter) Mittag-Leffer function is defined by the
+#' power series 
 #'     \deqn{E_(\alpha,\beta) (z) = \sum_(k=0)^(\inf) (z^k)/\Gamma(\alpha 
 #'     k + \beta) }
 #' for complex \eqn{z} and complex \eqn{\alpha, \beta} with 
 #' \eqn{Real(\alpha) > 0}.
 #'
-#' The Mittag-Leffler CDF is given by 
+#' The **first type** of Mittag-Leffler distribution assumes the Mittag-Leffler
+#' function as its tail function, so that the CDF is given by 
 #' \deqn{F(t; \alpha, \delta) = 1 - E_(\alpha,1) (-(t/\delta)^\alpha)}
 #' for \eqn{t \ge 0}, \eqn{0 < \alpha \le 1} 
 #' and scale parameter \eqn{\delta > 0}.
-#'
-#' The Mittag-Leffler PDF is given by 
+#' Its PDF is given by 
 #' \deqn{f(t; \alpha, \delta) = (t^(\alpha - 1))/(\delta^\alpha) 
-#' E_(\alpha,\alpha) [-(t/\delta)^\alpha]}
-#' for \eqn{t \ge 0}, \eqn{0 < \alpha \le 1} 
-#' and scale parameter \eqn{\delta > 0}.
+#' E_(\alpha,\alpha) [-(t/\delta)^\alpha].}
+#' For \eqn{\alpha = 1}, the Mittag-Leffler is identical to the expnoential
+#' distribution. For \eqn{0 < \alpha < 1}, it is (very) heavy-tailed, i.e.
+#' has infinite mean. 
+#' 
+#' The **second type** of Mittag-Leffler distribution is defined via its
+#' Laplace transform: 
+#' \deqn{\int_0^{\infty} e^{-st} f(t; \alpha, 1) dt = E_{\alpha,1}(-s)}
+#' It is light-tailed, i.e. all its moments are finite. 
+#' 
 #' @return \code{dml} returns the density, 
 #'         \code{pml} returns the distribution function, 
 #'         \code{qml} returns the quantile function, and 
 #'         \code{rml} generates random values.
-#' @source Uses Garrapa's code...
-#' @references Garrapa's paper and others...
-#' @seealso Anything here?
+#' @references
+#' Garrappa, R. (2015). Numerical Evaluation of Two and Three Parameter 
+#' Mittag-Leffler Functions. SIAM Journal on Numerical Analysis, 53(3),
+#'  1350–1369. \url{http://doi.org/10.1137/140971191}
+#'  
+#' Mittag-Leffler distribution. (2017, May 3). 
+#' In Wikipedia, The Free Encyclopedia.
+#' \url{https://en.wikipedia.org/w/index.php?title=Mittag-Leffler_distribution&oldid=778429885}
+#' 
+#' The Mittag-Leffler function. MathWorks File Exchange. 
+#' \url{https://au.mathworks.com/matlabcentral/fileexchange/48154-the-mittag-leffler-function}
+
 #' @name MittagLeffler
 NULL
 
@@ -44,53 +63,105 @@ NULL
 #' @examples
 #' dml(1, 0.8)
 #' @export
-dml <- function(t,a,d=1,log=FALSE) {
-  ml <- (t^(a-1)/(d^a))*mlf(-(t/d)^a, a, a, 1)
-  if(log==FALSE) {
-    return(ml)
-  }
-  else {
-    return(log(ml))
+dml <- function(t,a,d=1,p.log=FALSE, second.type=FALSE){
+  if (second.type==FALSE) {
+    return(dml1(t,a,d,p.log))
+  } else {
+    return(dml2(t,a,d,p.log))
   }
 }
 
+# first type
+dml1 <- function(t,a,d=1,p.log=FALSE) {
+  ml <- (t^(a-1)/(d^a))*mlf(-(t/d)^a, a, a, 1)
+  if (p.log) {
+    ml <- log(ml)
+  }
+  return(ml)
+}
+
+# second type
+dml2 <- function(u,a,d=1,p.log=FALSE) {
+  # find the distribution of E(d^(1/a)), where E() is the inverse stable
+  # subordinator; Meerschaert & Straka, Eq.(9)
+  t = d^(1/a)
+  h=(t/a)*u^(-1-1/a)*stabledist::dstable(t*u^(-1/a), alpha=a, beta=1.0, 
+                             gamma=1.0, delta=0.0, pm=1)
+  if (p.log) {
+    h <- log(h)
+  }
+  return(h)
+}
 
 
 #' @rdname MittagLeffler
 #' @examples
 #' pml(2, 0.7, 1.5)
 #' @export
-pml <- function(t,a,d=1,lower.tail=TRUE, log.p=FALSE) {
-  ml <- 1 - mlf(-(t/d)^a,a,1,1)
-  if(lower.tail==FALSE) {ml <- 1 - ml}
-  if(log.p==FALSE) {
-    return(ml)
+pml <- function(q,a,d=1, second.type=FALSE, lower.tail=TRUE, log.p=FALSE) {
+  # rescale
+  q <- q/d
+  if (!second.type){
+    p <- pml1(q,a)
+  } else {
+    p <- pml2(q,a)
   }
-  else {
-    return(log(ml))
+  if (!lower.tail) {
+    p <- 1-p
   }
+  if (log.p) {
+    p <- log(p)
+  }
+  return(p)
 }
 
+# type 1 with unit scale
+pml1 <- function(q,a) {
+  p <- 1 - mlf(-(q)^a,a,1,1)
+}
 
+# type 2 with unit scale
+pml2 <- function(q,a) {
+  p <- stabledist::pstable(q^(-1/a), alpha=a, beta=1, gamma=1, delta=0, 
+                           pm=1, lower.tail = FALSE)
+}
 
 #' @rdname MittagLeffler
 #' @examples
 #' qml(0.25, 0.9)
 #' @export
-qml <- function(p,a,d=1,lower.tail=TRUE, log.p=FALSE) {
-  if(log.p==TRUE) {
+
+qml <- function(p, a, d=1, second.type=FALSE, lower.tail=TRUE, 
+                log.p=FALSE) {
+  if (log.p) {
     p <- exp(p)
   }
-  if(lower.tail==FALSE) {p <- 1 - p}
+  if (!lower.tail) {
+    p <- 1-p
+  }
+  if (!second.type) {
+    q <- qml1(p, a, d)
+  } else {
+    q <- d * qml2(p, a)
+  }
+  return(q)
+}
+
+qml1 <- function(p,a,d=1) {
   x <- numeric(length(p))
   for (i in 1:length(p)) {
     qml_p <- function(t) {pml(t,a,d) - p[i]}
-    x[i] <- stats::uniroot(qml_p, interval = c(10^-14,100), extendInt="upX")$root
+    x[i] <- stats::uniroot(qml_p, interval = c(10^-14,100), 
+                           extendInt="upX")$root
   }
   return(x)
 }
 
-
+# type 2 with unit scale
+qml2 <- function(p, a){
+  q <- stabledist::qstable(p, alpha=a, beta=1, gamma=1, delta=0, pm=1,
+                           lower.tail=FALSE)^(-a)
+}
 
 #' @rdname MittagLeffler
 #' @examples
@@ -112,13 +183,22 @@ qml <- function(p,a,d=1,lower.tail=TRUE, log.p=FALSE) {
 #'   print(paste("alpha =", ml_a, "delta =", ml_d))
 #' }
 #' @export
-rml <- function(n,a,d=1){
-  x <- numeric(n)
-  u <- stats::runif(n)
-  for (i in 1:n) {
-    pml_u <- function(t) {pml(t,a,d) - u[i]}
-    x[i] <- stats::uniroot(pml_u, interval = c(10^-14,100), extendInt="upX")$root
+rml <- function(n,a,d=1, second.type=FALSE){
+  if (!second.type){
+    x <- d * rml1(n,a)
+  } else {
+    x <- d * rml2(n,a)
   }
   return(x)
 }
 
+# unit scale; see e.g. Haubold, Mathai & Saxena (2011)
+rml1 <- function(n, a){
+  y <- stabledist::rstable(n, alpha=a, beta=1, gamma=1, delta=0, pm=1)
+  x <- stats::rexp(n)
+  y * x^(1/a)
+}
+
+rml2 <- function(n, a){
+  stabledist::rstable(n, alpha=a, beta=1, gamma=1, delta=0, pm=1)^(-a)
+}
