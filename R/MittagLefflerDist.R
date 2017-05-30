54e424ddd028035d32f4af1e60bcda8f675d3441
Peter Straka <p.straka@unsw.edu.au>
2017-05-30 13:48:17 +1000

    Added vignette on calculation of parameters

--- R/MittagLefflerDist.R
+++ R/MittagLefflerDist.R
@@ -1,7 +1,7 @@
 #' Mittag-Leffler Distribution
 #'
-#' These functions provide probability density, cumulative distribution
-#' function, quantile function and random variate generation for the 
+#' Probability density, cumulative distribution
+#' function, quantile function and random variate generation for the
 #' two types of Mittag-Leffler distribution.
 #'
 #' @param t vector of quantiles
@@ -10,49 +10,49 @@
 #' @param d delta (scale) parameter of Mittag-Leffler distribution
 #' @param n Number of random values
 #' @param log,log.p logical; if TRUE, probabilities p are given as log(p)
-#' @param lower.tail logical; if TRUE, probabilities are P(X ≤ x) 
+#' @param lower.tail logical; if TRUE, probabilities are P(X ≤ x)
 #'     otherwise, P(X > x)
 #' @param second.type logical; if FALSE, first type of Mittag-Leffler
 #'     distribution is assumed
 #' @details
 #' The generalized (two-parameter) Mittag-Leffer function is defined by the
-#' power series 
-#'     \deqn{E_(\alpha,\beta) (z) = \sum_(k=0)^(\inf) (z^k)/\Gamma(\alpha 
+#' power series
+#'     \deqn{E_(\alpha,\beta) (z) = \sum_(k=0)^(\inf) (z^k)/\Gamma(\alpha
 #'     k + \beta) }
-#' for complex \eqn{z} and complex \eqn{\alpha, \beta} with 
+#' for complex \eqn{z} and complex \eqn{\alpha, \beta} with
 #' \eqn{Real(\alpha) > 0}.
 #'
 #' The **first type** of Mittag-Leffler distribution assumes the Mittag-Leffler
-#' function as its tail function, so that the CDF is given by 
+#' function as its tail function, so that the CDF is given by
 #' \deqn{F(t; \alpha, \delta) = 1 - E_(\alpha,1) (-(t/\delta)^\alpha)}
-#' for \eqn{t \ge 0}, \eqn{0 < \alpha \le 1} 
+#' for \eqn{t \ge 0}, \eqn{0 < \alpha \le 1}
 #' and scale parameter \eqn{\delta > 0}.
-#' Its PDF is given by 
-#' \deqn{f(t; \alpha, \delta) = (t^(\alpha - 1))/(\delta^\alpha) 
+#' Its PDF is given by
+#' \deqn{f(t; \alpha, \delta) = (t^(\alpha - 1))/(\delta^\alpha)
 #' E_(\alpha,\alpha) [-(t/\delta)^\alpha].}
 #' For \eqn{\alpha = 1}, the Mittag-Leffler is identical to the expnoential
 #' distribution. For \eqn{0 < \alpha < 1}, it is (very) heavy-tailed, i.e.
-#' has infinite mean. 
-#' 
+#' has infinite mean.
+#'
 #' The **second type** of Mittag-Leffler distribution is defined via its
-#' Laplace transform: 
+#' Laplace transform:
 #' \deqn{\int_0^{\infty} e^{-st} f(t; \alpha, 1) dt = E_{\alpha,1}(-s)}
-#' It is light-tailed, i.e. all its moments are finite. 
-#' 
-#' @return \code{dml} returns the density, 
-#'         \code{pml} returns the distribution function, 
-#'         \code{qml} returns the quantile function, and 
-#'         \code{rml} generates random values.
+#' It is light-tailed, i.e. all its moments are finite.
+#'
+#' @return \code{dml} returns the density,
+#'         \code{pml} returns the distribution function,
+#'         \code{qml} returns the quantile function, and
+#'         \code{rml} generates random variables.
 #' @references
-#' Garrappa, R. (2015). Numerical Evaluation of Two and Three Parameter 
+#' Garrappa, R. (2015). Numerical Evaluation of Two and Three Parameter
 #' Mittag-Leffler Functions. SIAM Journal on Numerical Analysis, 53(3),
 #'  1350–1369. \url{http://doi.org/10.1137/140971191}
-#'  
-#' Mittag-Leffler distribution. (2017, May 3). 
+#'
+#' Mittag-Leffler distribution. (2017, May 3).
 #' In Wikipedia, The Free Encyclopedia.
 #' \url{https://en.wikipedia.org/w/index.php?title=Mittag-Leffler_distribution&oldid=778429885}
-#' 
-#' The Mittag-Leffler function. MathWorks File Exchange. 
+#'
+#' The Mittag-Leffler function. MathWorks File Exchange.
 #' \url{https://au.mathworks.com/matlabcentral/fileexchange/48154-the-mittag-leffler-function}
 
 #' @name MittagLeffler
@@ -85,7 +85,7 @@ dml2 <- function(u,a,d=1,p.log=FALSE) {
   # find the distribution of E(d^(1/a)), where E() is the inverse stable
   # subordinator; Meerschaert & Straka, Eq.(9)
   t = d^(1/a)
-  h=(t/a)*u^(-1-1/a)*stabledist::dstable(t*u^(-1/a), alpha=a, beta=1.0, 
+  h=(t/a)*u^(-1-1/a)*stabledist::dstable(t*u^(-1/a), alpha=a, beta=1.0,
                              gamma=1.0, delta=0.0, pm=1)
   if (p.log) {
     h <- log(h)
@@ -122,7 +122,7 @@ pml1 <- function(q,a) {
 
 # type 2 with unit scale
 pml2 <- function(q,a) {
-  p <- stabledist::pstable(q^(-1/a), alpha=a, beta=1, gamma=1, delta=0, 
+  p <- stabledist::pstable(q^(-1/a), alpha=a, beta=1, gamma=1, delta=0,
                            pm=1, lower.tail = FALSE)
 }
 
@@ -131,7 +131,7 @@ pml2 <- function(q,a) {
 #' qml(0.25, 0.9)
 #' @export
 
-qml <- function(p, a, d=1, second.type=FALSE, lower.tail=TRUE, 
+qml <- function(p, a, d=1, second.type=FALSE, lower.tail=TRUE,
                 log.p=FALSE) {
   if (log.p) {
     p <- exp(p)
@@ -151,7 +151,7 @@ qml1 <- function(p,a,d=1) {
   x <- numeric(length(p))
   for (i in 1:length(p)) {
     qml_p <- function(t) {pml(t,a,d) - p[i]}
-    x[i] <- stats::uniroot(qml_p, interval = c(10^-14,100), 
+    x[i] <- stats::uniroot(qml_p, interval = c(10^-14,100),
                            extendInt="upX")$root
   }
   return(x)
